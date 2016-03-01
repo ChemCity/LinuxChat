1e596b7c975d998a3d842bfc6128a28e77c56b5b
eigenket1 <tomtang_13@hotmail.com>
2016-02-29 18:57:49 -0800

    initial linux chat program

---
+++ a3c.cpp
@@ -0,0 +1,94 @@
+
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define SERVER_TCP_PORT		7000	// Default port
+#define BUFLEN			80  	// Buffer length
+
+//DONT FORGET TO ADD COMMAND LINE ARGUMENT FOR CHAT SESSION FILE DUMP
+int main (int argc, char **argv){
+	int n, bytes_to_read;
+	int sd, port;
+	struct hostent	*hp;
+	struct sockaddr_in server;
+	char  *host, *bp, rbuf[BUFLEN], tempbuf[BUFLEN], sbuf[BUFLEN], **pptr, *name;
+	char str[16];
+
+	switch(argc){
+		case 3:
+			host =	argv[1];	// Host name
+			name =  argv[2];
+			port =	SERVER_TCP_PORT;
+		break;
+		case 4:
+			host =	argv[1];
+			name =  argv[2];
+			port =	atoi(argv[3]);	// User specified port
+		break;
+		default:
+			fprintf(stderr, "Usage: %s host username [port]\n", argv[0]);
+			exit(1);
+	}
+
+	// Create the socket
+	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+		perror("Cannot create socket");
+		exit(1);
+	}
+	bzero((char *)&server, sizeof(struct sockaddr_in));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	if ((hp = gethostbyname(host)) == NULL){
+		fprintf(stderr, "Unknown server address\n");
+		exit(1);
+	}
+	bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+
+	// Connecting to the server
+	if (connect (sd, (struct sockaddr *)&server, sizeof(server)) == -1){
+		fprintf(stderr, "Can't connect to server\n");
+		perror("connect");
+		exit(1);
+	}
+	printf("Connected:    Server Name: %s\n", hp->h_name);
+	pptr = hp->h_addr_list;
+	printf("\t\tIP Address: %s\n", inet_ntop(hp->h_addrtype, *pptr, str, sizeof(str)));
+	
+	
+	if(fork() == 0){
+		//Sending process
+		while(1){
+			// MAKE SMALL CHANGE HERE
+			fgets (tempbuf, BUFLEN, stdin);
+			sprintf(sbuf,"%s: %s",name, tempbuf);
+			// Transmit data through the socket
+			send (sd, sbuf, BUFLEN, 0);
+		}
+	} else {
+		//Receiving process
+		while(1){
+			bp = rbuf;
+			bytes_to_read = BUFLEN;
+
+			// client makes repeated calls to recv until no more data is expected to arrive.
+			n = 0;
+			while ((n = recv (sd, bp, bytes_to_read, 0)) < BUFLEN){
+				bp += n;
+				bytes_to_read -= n;
+			}
+			// MAKE SMALL CHANGE HERE AS WELL
+			printf ("%s\n", rbuf);
+			fflush(stdout);
+		}
+	}
+	close (sd);
+	return (0);
+}
