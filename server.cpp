63dd440408c61d0fe412b12f973ff9c805ec8281
Gabriella Cheung <gabriella.bcit@gmail.com>
2016-03-23 16:32:34 -0700

    Added message on server and added makefile for server

--- server.cpp
+++ server.cpp
@@ -1,4 +1,4 @@
-
+ 
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -116,7 +116,7 @@ int main (int argc, char **argv) {
 	FD_ZERO(&allset);
 	FD_SET(listen_sd, &allset);
 
-
+	printf("Server is ready for incoming connections on port %d\n", SERVER_TCP_PORT); 
 	while (1){
 		rset = allset;               // structure assignment
 		nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
